baa6b4d4cf1342e2c72663a412c242855a6ee6f4
dariaslota <dslota@kse.org.ua>
2025-05-08 19:41:09 +0300

    практика 2

---
+++ lecture_2/6.py
@@ -0,0 +1,4 @@
+height = int(input("Введіть свій зріст "))
+weight = int(input("Введіть свою вагу "))
+bmi = (weight**2)/height
+print("При вазі", weight, "кг і рості", height, "метрів Ваш ВМІ складає", bmi)
